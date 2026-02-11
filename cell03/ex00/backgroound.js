98d356e0aa947043cd00ca882a84552a6fffd22c
Mariii <139721644+mariiiporfirio@users.noreply.github.com>
2026-02-10 21:41:42 -0300

    Delete cell03 directory

--- cell03/ex00/backgroound.js
+++
@@ -1,6 +0,0 @@
-let selecao = document.getElementById("botao");
-
-selecao.addEventListener("click", () =>{
-    let color = "red";
-    document.body.style.background = color;
-})
--- cell03/ex04/ex00bis/backgroound.js
+++
@@ -1,9 +0,0 @@
-$('#botao').on('click', function() {
-    let color = "red";
-    $('body').css('background', color);
-});
-
-$('#botao').on('mouseleave', () =>{
-    let color = "pink";
-    $('body').css('background', color);
-})
