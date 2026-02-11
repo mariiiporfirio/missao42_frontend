aceb4e42b3b881a047a9575378a46e05a173653c
Mariii <139721644+mariiiporfirio@users.noreply.github.com>
2026-02-10 21:41:04 -0300

    Delete cell00 directory

--- cell00/ex04/argv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [ -z "$*" ]; then echo "No arguments supplied"; fi
-echo "$1
-$2
-$3"
--- cell00/ex05/build.sh
+++
@@ -1,1 +0,0 @@
-if [ -z "$*" ]; then echo "No arguments supplied"; else for i in "$@"; do mkdir "ex$i"; done; fi
